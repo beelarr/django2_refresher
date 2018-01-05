733938f4cb169d998990a4eca61f582e2d9b5aae
beelarr <bryonl@me.com>
2018-01-05 17:44:31 -0600

    Fix url path

--- first_project/urls.py
+++ first_project/urls.py
@@ -19,8 +19,7 @@ from first_app import views
 from django.conf.urls import include
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('first_app/', include('first_app.urls')),
+    path('', include('first_app.urls')),
     path('admin/', admin.site.urls),
 
 ]
